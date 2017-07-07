285272e1d4952dea2ce5af7b6986bdf5866fde2b
Cosmin Ionita <ionita.cosmin@outlook.com>
2017-07-07 14:48:52 +0300

    Removed tab and added proxy checking

--- apps-pending/django/ubuntu/install_dw.sh
+++ apps-pending/django/ubuntu/install_dw.sh
@@ -7,148 +7,158 @@ check_root_privilege
 
 # If we have a proxy, then we set it up
 if [ "$#" -eq 1 ]  && check_proxy_parameter $1; then
-
-  curl --proxy $1 https://www.apache.org/dist/cassandra/KEYS | apt-key add -
-  curl --proxy $1 -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
+  echo "Proxy information was supplied correctly. Continuing with proxy settings..."
   set_proxy $1
 
 # If we don't have a proxy, we continue normally
 else
+  echo "Proxy information was supplied incorrectly (or not supplied at all). Continuing without proxy settings..."
+
   curl https://www.apache.org/dist/cassandra/KEYS | apt-key add -
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 fi
-  # Add repositories
-  echo -e "\n\nAdd apt repositories ..."
-  add-apt-repository ppa:webupd8team/java
-  echo "deb http://www.apache.org/dist/cassandra/debian 310x main" | \
-       tee -a /etc/apt/sources.list.d/cassandra.sources.list
+# Add repositories
+echo -e "\n\nAdd apt repositories ..."
+add-apt-repository ppa:webupd8team/java
+echo "deb http://www.apache.org/dist/cassandra/debian 310x main" | \
+     tee -a /etc/apt/sources.list.d/cassandra.sources.list
 
-  apt-key fingerprint 0EBFCD88
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) stable"
-  apt-get update
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable"
+apt-get update
 
-  # Install packages
-  echo -e "\n\nInstall packages ..."
-  apt-get install -y software-properties-common oracle-java8-installer    \
-      cassandra memcached apt-transport-https ca-certificates docker-ce   \
-      build-essential git libmemcached-dev python3-virtualenv python3-dev \
-      zlib1g-dev siege curl
+# Install packages
+echo -e "\n\nInstall packages ..."
+apt-get install -y software-properties-common oracle-java8-installer    \
+    cassandra memcached apt-transport-https ca-certificates docker-ce   \
+    build-essential git libmemcached-dev python3-virtualenv python3-dev \
+    zlib1g-dev siege curl
 
-  echo -e "\n\nDocker pull graphite image ..."
-  docker pull hopsoft/graphite-statsd
+echo -e "\n\nDocker pull graphite image ..."
+docker pull hopsoft/graphite-statsd
 
-  #Initialize docker container
-  echo -e "\n\nInitialize container ..."
-  docker run -d               \
-      --name graphite         \
-      --restart=always        \
-      -p 80:80                \
-      -p 2003-2004:2003-2004  \
-      -p 2023-2024:2023-2024  \
-      -p 8125:8125/udp        \
-      -p 8126:8126            \
-      hopsoft/graphite-statsd
+#Initialize docker container
+echo -e "\n\nInitialize container ..."
+docker run -d               \
+    --name graphite         \
+    --restart=always        \
+    -p 80:80                \
+    -p 2003-2004:2003-2004  \
+    -p 2023-2024:2023-2024  \
+    -p 8125:8125/udp        \
+    -p 8126:8126            \
+    hopsoft/graphite-statsd
 
-  # Disable services
-  echo -e "\n\nDisable services ..."
-  systemctl disable memcached.service
-  systemctl disable cassandra.service
-  systemctl disable docker.service
+# Disable services
+echo -e "\n\nDisable services ..."
+systemctl disable memcached.service
+systemctl disable cassandra.service
+systemctl disable docker.service
 
-  # Sets up a memcached server with 5 GB memory
-  # Check if system it's having 5 GB memory
-  echo -e "\n\nCheck system memory ..."
-  mem_total_MB=$(free -m | grep Mem | awk '{print $2}')
-  mem_av_MB=$(free -m | grep Mem | awk '{print $NF}')
+# Sets up a memcached server with 5 GB memory
+# Check if system it's having 5 GB memory
+echo -e "\n\nCheck system memory ..."
+mem_total_MB=$(free -m | grep Mem | awk '{print $2}')
+mem_av_MB=$(free -m | grep Mem | awk '{print $NF}')
 
-  if [ "$mem_total_MB" -ge "5120" ]; then
-      if [ "$mem_av_MB" -ge "2048" ]; then
-          echo "Found enough memory on the system! [OK]"
-      else
-          echo "Not enough available memory on the system! [FAIL]"
-          exit 2
-      fi
-  else
-      echo "Not enough memory in the system! [FAIL]"
-      exit 3
-  fi
+if [ "$mem_total_MB" -ge "5120" ]; then
+    if [ "$mem_av_MB" -ge "2048" ]; then
+        echo "Found enough memory on the system! [OK]"
+    else
+        echo "Not enough available memory on the system! [FAIL]"
+        exit 2
+    fi
+else
+    echo "Not enough memory in the system! [FAIL]"
+    exit 3
+fi
 
-  echo -e "\n\nClone django-workload repository ..."
-  su "$SUDO_USER" -c "git clone https://github.com/Instagram/django-workload"
+echo -e "\n\nClone django-workload repository ..."
+su "$SUDO_USER" -c "git clone https://github.com/Instagram/django-workload"
 
-  # Config memcached
-  # Backup old memcached config file
-  if [ -f /etc/memcached.conf ]; then
-      mv /etc/memcached.conf /etc/memcached.conf.old
-      echo -e "\n\nBackup /etc/memcached.conf to /etc/memcached.conf.old"
-  fi
+# Config memcached
+# Backup old memcached config file
+if [ -f /etc/memcached.conf ]; then
+    mv /etc/memcached.conf /etc/memcached.conf.old
+    echo -e "\n\nBackup /etc/memcached.conf to /etc/memcached.conf.old"
+fi
 
-  . memcached.cfg
+. memcached.cfg
 
-  echo -e "\n\nWrite memcached config file ..."
-  cat > /etc/memcached.conf <<- EOF
-  	# Daemon mode
-  	-d
-  	logfile /var/log/memcached.log
-  	-m "$MEMORY"
-  	-p "$PORT"
-  	-u "$USER"
-  	-l "$LISTEN"
-  EOF
+echo -e "\n\nWrite memcached config file ..."
+cat > /etc/memcached.conf <<- EOF
+	# Daemon mode
+	-d
+	logfile /var/log/memcached.log
+	-m "$MEMORY"
+	-p "$PORT"
+	-u "$USER"
+	-l "$LISTEN"
+EOF
 
-  echo -e "\n\nCreate python virtual environment ..."
-  (
-  su "$SUDO_USER" -c                                    \
-  "cd django-workload/django-workload || exit 4        ;\
-  python3 -m virtualenv -p python3 venv                ;\
-  source venv/bin/activate                             ;\
-  pip install -r requirements.txt                      ;\
-  deactivate                                           ;\
-  cp cluster_settings_template.py cluster_settings.py"
-  )
+echo -e "\n\nCreate python virtual environment ..."
+(
+su "$SUDO_USER" -c                                    \
+"cd django-workload/django-workload || exit 4        ;\
+python3 -m virtualenv -p python3 venv                ;\
+source venv/bin/activate                             ;\
+pip install -r requirements.txt                      ;\
+deactivate                                           ;\
+cp cluster_settings_template.py cluster_settings.py"
+)
 
-  echo -e "\n\nGenerate siege urls file ..."
-  (
-  su "$SUDO_USER" -c                    \
-  "cd django-workload/client || exit 5; \
-  ./gen-urls-file"
-  )
+echo -e "\n\nGenerate siege urls file ..."
+(
+su "$SUDO_USER" -c                    \
+"cd django-workload/client || exit 5; \
+./gen-urls-file"
+)
 
-  # Set cores count to uwsgi.ini
-  (
-  su "$SUDO_USER" -c                             \
-  "cd django-workload/django-workload || exit 4; \
-  sed -i 's/processes = 4/processes = $(grep -c processor /proc/cpuinfo)/g' uwsgi.ini"
-  )
+# Set cores count to uwsgi.ini
+(
+su "$SUDO_USER" -c                             \
+"cd django-workload/django-workload || exit 4; \
+sed -i 's/processes = 4/processes = $(grep -c processor /proc/cpuinfo)/g' uwsgi.ini"
+)
 
-  # Append client settings to /etc/sysctl.conf
-  echo -e "\nWrite sysctl settings ..."
-  cat >> /etc/sysctl.conf <<- EOF
-  	net.ipv4.tcp_tw_reuse=1
-  	net.ipv4.ip_local_port_range=1024 64000
-  	net.ipv4.tcp_fin_timeout=45
-  	net.core.netdev_max_backlog=10000
-  	net.ipv4.tcp_max_syn_backlog=12048
-  	net.core.somaxconn=1024
-  	net.netfilter.nf_conntrack_max=256000
-  EOF
+# Append client settings to /etc/sysctl.conf
+echo -e "\nWrite sysctl settings ..."
+cat >> /etc/sysctl.conf <<- EOF
+	net.ipv4.tcp_tw_reuse=1
+	net.ipv4.ip_local_port_range=1024 64000
+	net.ipv4.tcp_fin_timeout=45
+	net.core.netdev_max_backlog=10000
+	net.ipv4.tcp_max_syn_backlog=12048
+	net.core.somaxconn=1024
+	net.netfilter.nf_conntrack_max=256000
+EOF
 
-  echo -e "\n\nAdd nf_conntrack to modules ..."
-  echo "nf_conntrack" >> /etc/modules
+echo -e "\n\nAdd nf_conntrack to modules ..."
+echo "nf_conntrack" >> /etc/modules
 
-  echo -e "\n\nAdd limits settings ..."
-  cat >> /etc/security/limits.conf <<- EOF
-  	* soft nofile 1000000
-  	* hard nofile 1000000
-  EOF
+echo -e "\n\nAdd limits settings ..."
+cat >> /etc/security/limits.conf <<- EOF
+	* soft nofile 1000000
+	* hard nofile 1000000
+EOF
 
-  # Create siegerc
-  echo -e "\n\nCreate siegerc ..."
-  su - "$SUDO_USER" -c "echo 'failures = 1000000' > .siegerc"
+# Create siegerc
+echo -e "\n\nCreate siegerc ..."
+su - "$SUDO_USER" -c "echo 'failures = 1000000' > .siegerc"
 
-  echo -e "\n\n"
-  # Modifying limits.conf requires system reboot
-  read -rsn1 -p "Press any key to reboot"
-  reboot
+echo -e "\n\n"
+# Modifying limits.conf requires system reboot
+read -rsn1 -p "Press any key to reboot"
+reboot
+	* hard nofile 1000000
+EOF
+
+# Create siegerc
+echo -e "\n\nCreate siegerc ..."
+su - "$SUDO_USER" -c "echo 'failures = 1000000' > .siegerc"
+
+echo -e "\n\n"
+# Modifying limits.conf requires system reboot
+read -rsn1 -p "Press any key to reboot"
+reboot
