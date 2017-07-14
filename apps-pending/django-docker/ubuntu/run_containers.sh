7d4b3a56e72f588c5d057a5cfc55a220987764b6
Cosmin Ionita <ionita.cosmin@outlook.com>
2017-07-14 18:14:36 +0300

    Added run_containers demonstration script

---
+++ apps-pending/django-docker/ubuntu/run_containers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+################################### Description ########################################
+#
+# This script will start a docker container for each service necessary in the django workload
+# It is created only for demonstration purposes, to see how we should power on a docker container.
+# In a production environment, we'll use a tool that will manage our containers automatically.
+#
+########################################################################################
+
+
+# Start memcached container
+docker run rintech/webtierbench:memcache-webtier
+
+# Start cassandra container
+docker run -e CASSANDRA_ENDPOINT='<ip>:<port>' -e MEMCACHED_ENDPOINT='<ip>:<port>' rintech/webtierbench:cassandra-webtier
+
+# Start uwsgi container
+docker run -e ATTEMPTS='<no_attempts>' rintech/webtierbench:uwsgi-webtier
