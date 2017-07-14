e3e4f53066c609d08aadb8ed9393f5d1e5427dd9
Cosmin Ionita <ionita.cosmin@outlook.com>
2017-07-14 17:57:11 +0300

    Added siege_init script logic

--- apps-pending/django-docker/ubuntu/siege/siege_init.sh
+++ apps-pending/django-docker/ubuntu/siege/siege_init.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-# This script will configure and start the siege service inside a docker
-# container based on Ubuntu 16.04
+################################### Description ########################################
+#
+# This script will be called when the siege container is run (powered on / executed).
+# When we run the siege container, we need to pass the number of runs that siege needs to perform
+# We will do this passing using environment variables that we set when the docker run command is run
+# and we read them in this script.
+# For example, consider following command:
+#
+#   docker run -e ATTEMPTS=10 siege-webtier
+#
+# In this script, we will have access to the RUNS variable.
+#
+########################################################################################
 
-# Start siege and keep the process running
-cd django-workload/client && ./run-siege.sh && tail -F /dev/null
+#### This script is currently under development ####
+
+# Set the working directory
+cd django-workload/client
+
+# Run siege $ATTEMPTS times
+for (( i=1; i<=$ATTEMPTS; i++ ))
+	do
+	   ./run-siege.sh
+done
+
+# Keep the siege process running
+tail -F /dev/null
