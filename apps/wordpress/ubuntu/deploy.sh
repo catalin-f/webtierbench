c8600ab3bbd97f5a43f8b5bb262048bef3eeeac0
Catalin Fierut <catalin.fierut@gmail.com>
2017-07-18 18:20:32 +0300

    Added wordpress

---
+++ apps/wordpress/ubuntu/deploy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -x
+. ../../common_func.sh
+
+###############################################################################
+# Environment data
+###############################################################################
+WEBTIER_HTTP_PROXY=${WEBTIER_HTTP_PROXY}
+
+
+###############################################################################
+# Commands
+###############################################################################
+
+check_root_privilege
+
+http_proxy="${WEBTIER_HTTP_PROXY}" https_proxy="${WEBTIER_HTTP_PROXY}" \
+    add-apt-repository "deb http://dl.hhvm.com/ubuntu xenial main"
+
+# Install packages for oss performance
+http_proxy="${WEBTIER_HTTP_PROXY}" https_proxy="${WEBTIER_HTTP_PROXY}"  apt-get install -y \
+    nginx \
+    unzip \
+    coreutils \
+    autotools-dev\
+    autoconf \
+	hhvm
+
+echo 'Checking if oss-performance is already installed:'
+if [ -d "$oss_dir" ]; then
+	rm -rf "$oss_dir"
+	echo ' - oss-performance was found and removed'
+	echo '************************************************************'
+else
+	echo ' - oss-performance was not found'
+	echo '************************************************************'
+fi
+
+
+su $SUDO_USER -c "HTTPS_PROXY=${WEBTIER_HTTP_PROXY}    \
+git clone https://github.com/hhvm/oss-performance;		\
+cd $oss_dir;							\
+wget https://getcomposer.org/installer -O composer-setup.php;	\
+hhvm composer-setup.php;					\
+hhvm composer.phar install"
+
+start_service "mysql"
+systemctl stop nginx.service
+systemctl disable nginx.service
