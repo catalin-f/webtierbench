78630f956d26ddda8df0d4506fcf7a4676e0cb4a
Cosmin Ionita <ionita.cosmin@outlook.com>
2017-07-20 18:29:57 +0300

    Refactored siege init script

--- apps-pending/django-docker/ubuntu/siege/siege_init.sh
+++ apps-pending/django-docker/ubuntu/siege/siege_init.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
 
-################################### Description ########################################
-#
-# This script will be called when the siege container is run (powered on / executed).
-# When we run the siege container, we need to pass the number of runs that siege needs to perform
-# We will do this passing using environment variables that we set when the docker run command is run
-# and we read them in this script.
-# For example, consider following command:
-#
-#   docker run -e ATTEMPTS=10 -e TARGET_ENDPOINT='192.168.0.1:5050' siege-webtier
-#
-# In this script, we will have access to the ATTEMPTS variable.
-#
-########################################################################################
+# This script is called each time the siege container is run
 
-#### This script is currently under development ####
-
-# Set the working directory
 cd /django-workload/client || exit 1
 
-# Update the target endpoint
 sed -i "s/localhost/$TARGET_ENDPOINT/g" urls_template.txt
 
-# Generate the urls with the updated endpoint
 ./gen-urls-file
 
-# Run siege $ATTEMPTS times
 for (( i=1; i<=ATTEMPTS; i++ ))
 	do
 	   ./run-siege.sh
 done
 
-# Keep the siege process running
 tail -f /dev/null
