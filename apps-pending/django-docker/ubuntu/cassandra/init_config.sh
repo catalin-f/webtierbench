19892e706b0ad9ba5700cc87d1157830c4cd3df3
Cosmin Ionita <ionita.cosmin@outlook.com>
2017-07-20 18:32:33 +0300

    Refactored cassandra init config script

--- apps-pending/django-docker/ubuntu/cassandra/init_config.sh
+++ apps-pending/django-docker/ubuntu/cassandra/init_config.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# This script is called when the cassandra image is built
+# It receives the hostname as a parameter (cassandra)
+
 echo -e "\n\nAdd limits settings ..."
 cat >> /etc/security/limits.conf <<- EOF
 	* soft nofile 1000000
 	* hard nofile 1000000
 EOF
 
-IP_ADDR=$1
+HOSTNAME=$1
 
-sed -i "s/listen_address: localhost/listen_address: $IP_ADDR/g" /etc/cassandra/cassandra.yaml
-sed -i "s/seeds: \"127.0.0.1\"/seeds: \"$IP_ADDR\"/g" /etc/cassandra/cassandra.yaml
-sed -i "s/rpc_address: localhost/rpc_address: $IP_ADDR/g" /etc/cassandra/cassandra.yaml
+sed -i "s/listen_address: localhost/listen_address: $HOSTNAME/g" /etc/cassandra/cassandra.yaml
+sed -i "s/seeds: \"127.0.0.1\"/seeds: \"$HOSTNAME\"/g" /etc/cassandra/cassandra.yaml
+sed -i "s/rpc_address: localhost/rpc_address: $HOSTNAME/g" /etc/cassandra/cassandra.yaml
