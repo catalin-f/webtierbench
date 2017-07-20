3ff5f31d743e0e43e8537bceb7f429a76d973556
Cosmin Ionita <ionita.cosmin@outlook.com>
2017-07-20 15:57:55 +0300

    Added network configuration to run_containers script

--- apps-pending/django-docker/ubuntu/run_containers.sh
+++ apps-pending/django-docker/ubuntu/run_containers.sh
@@ -8,15 +8,28 @@
 #
 ########################################################################################
 
+docker network create --opt com.docker.network.bridge.name=django --attachable -d bridge --gateway 10.10.10.1 --subnet 10.10.10.0/24 --ip-range 10.10.10.8/29 django_network
+
+echo "The network is up and running!"
 
 # Start memcached container
-docker run rintech/webtierbench:memcache-webtier
+docker run -tid -h memcached --name memcached_container --network django_network --ip 10.10.10.9 memcached-webtier
+
+echo "Memcached is up and running!"
 
 # Start cassandra container
-docker run rintech/webtierbench:cassandra-webtier
+docker run -tid --privileged -h cassandra --name cassandra_container --network django_network --ip 10.10.10.10 cassandra-webtier
+
+sleep 30
+echo "Cassandra is up and running!"
 
 # Start uwsgi container
-docker run -e CASSANDRA_ENDPOINT='<ip>:<port>' -e MEMCACHED_ENDPOINT='<ip>:<port>' rintech/webtierbench:uwsgi-webtier
+docker run -tid -h uwsgi --name uwsgi_container --network django_network --ip 10.10.10.11 -e CASSANDRA_ENDPOINT=10.10.10.10 -e MEMCACHED_ENDPOINT=10.10.10.9 uwsgi-webtier
+
+sleep 30
+echo "uWSGI is up and running!"
 
 # Start siege container
-docker run -e ATTEMPTS='<no_attempts>' -e TARGET_ENDPOINT='<ip>:<port>' rintech/webtierbench:siege-webtier
+docker run -ti -h siege --name siege_container --network django_network --ip 10.10.10.12 -e ATTEMPTS=10 -e TARGET_ENDPOINT=10.10.10.11 siege-webtier
+
+echo "Siege is up and running!"
