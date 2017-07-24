f1987a8976820b637035af10653ed502e6fbd362
Silviu Maciuceanu <silviu.maciuceanu@rinftech.com>
2017-07-24 15:28:08 +0300

    modified mysql script inside run_wordpress.sh

--- apps-pending/wordpress/ubuntu/run_wordpress.sh
+++ apps-pending/wordpress/ubuntu/run_wordpress.sh
@@ -14,9 +14,7 @@ done
 echo 1 | sudo tee /proc/sys/net/ipv4/tcp_tw_reuse
 chmod -R 775 /var/log/nginx
 
-mysql -u root -e "USE mysql;"
-mysql -u root -e "UPDATE user SET plugin='mysql_native_password' WHERE User='root';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "USE mysql; UPDATE user SET plugin='mysql_native_password' WHERE User='root'; FLUSH PRIVILEGES;"
 
 for (( i=1; i<=$1; i++ ))
 do
