4a3f7f1c9f8acbac9bae3cc37ef1e66d067cb2e6
Cosmin Ionita <ionita.cosmin@outlook.com>
2017-07-28 16:38:20 +0300

    Added django_docker control scripts

--- apps/django_docker/ubuntu/start.sh
+++ apps/django_docker/ubuntu/start.sh
@@ -9,4 +9,4 @@
 # Commands
 ###############################################################################
 
-
+docker run -tid -h uwsgi --name uwsgi_container --network django_network --ip 10.10.10.11 -e CASSANDRA_ENDPOINT=10.10.10.10 -e MEMCACHED_ENDPOINT=10.10.10.9 rinftech/webtierbench:uwsgi-webtier
