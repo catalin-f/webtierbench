f2421732899f93d6dd38e943b3cd026133170572
Alexandru-Cristian Anghel <alexandru.anghel@rinftech.com>
2017-07-28 16:42:34 +0300

    change /tmp owner on init

--- apps-pending/django-docker/ubuntu/siege/siege_init.sh
+++ apps-pending/django-docker/ubuntu/siege/siege_init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-su - tester -c "rm -rf /tmp/siege*                 \
-    && cd django-workload/client                   \
-    && LOG='/tmp/siege.log' ./run-siege" || exit 1
+rm -rf /tmp/siege*
+chown -R tester:tester /tmp
+
+su - tester -c "cd django-workload/client \
+    && LOG='/tmp/siege.log' ./run-siege"  \
+    || exit 1
 
 tail -f /dev/null
