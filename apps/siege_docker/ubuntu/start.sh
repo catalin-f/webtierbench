f85e3b0f0652c28cff5d49ae4e4a2d2a0ca0e0c6
Cosmin Ionita <ionita.cosmin@outlook.com>
2017-08-03 17:06:56 +0300

    Added django environment variable to siege docker start script

--- apps/siege_docker/ubuntu/start.sh
+++ apps/siege_docker/ubuntu/start.sh
@@ -6,7 +6,9 @@
 WEBTIER_SIEGE_WORKERS=${WEBTIER_SIEGE_WORKERS}
 WEBTIER_SIEGE_RUNMODE=${WEBTIER_SIEGE_RUNMODE}
 
+DJANGO_IP=${DJANGO_IP}
+
 ###############################################################################
 # Commands
 ###############################################################################
-docker run -ti -h siege --name siege_container --network django_network --ip 10.10.10.12 -e TARGET_ENDPOINT=10.10.10.11 rinftech/webtierbench:siege-webtier
+docker run -ti -h siege --name siege_container --network django_network --ip 10.10.10.12 -e TARGET_ENDPOINT=$DJANGO_IP rinftech/webtierbench:siege-webtier
