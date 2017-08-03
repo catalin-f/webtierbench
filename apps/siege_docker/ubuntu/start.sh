947c5c97a0a04bd40bb877babf8432a0a8b59adb
Cosmin Ionita <ionita.cosmin@outlook.com>
2017-08-03 14:07:30 +0300

    Added custom run environment variables on siege container

--- apps/siege_docker/ubuntu/start.sh
+++ apps/siege_docker/ubuntu/start.sh
@@ -3,10 +3,10 @@
 ###############################################################################
 # Environment data
 ###############################################################################
-
+WEBTIER_SIEGE_WORKERS=${WEBTIER_SIEGE_WORKERS}
+WEBTIER_SIEGE_RUNMODE=${WEBTIER_SIEGE_RUNMODE}
 
 ###############################################################################
 # Commands
 ###############################################################################
-
-docker run -ti -h siege --name siege_container --network django_network --ip 10.10.10.12 -e TARGET_ENDPOINT=10.10.10.11 rinftech/webtierbench:siege-webtier
+docker run -ti -h siege --name siege_container --network django_network --ip 10.10.10.12 -e TARGET_ENDPOINT=10.10.10.11 -e WORKERS=$WEBTIER_SIEGE_WORKERS -e RUNMODE=$WEBTIER_SIEGE_RUNMODE rinftech/webtierbench:siege-webtier
