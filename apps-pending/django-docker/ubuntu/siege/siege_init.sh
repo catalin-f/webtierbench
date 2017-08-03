eff729b9f6bafb54254a7a20ebdea907fabc3db9
Cosmin Ionita <ionita.cosmin@outlook.com>
2017-08-03 14:18:43 +0300

    Reverted changes regarding the custom number of runs

--- apps-pending/django-docker/ubuntu/siege/siege_init.sh
+++ apps-pending/django-docker/ubuntu/siege/siege_init.sh
@@ -5,19 +5,5 @@ chown -R tester:tester /tmp
 
 su - tester -c "cd django-workload/client                                    \
                 && sed -i 's/localhost/$TARGET_ENDPOINT/g' urls_template.txt \
-                && ./gen-urls-file"                                          \
-
-if [ -n "$RUNMODE" ]; then
-
-	    for (( i=1; i<=$WORKERS; i++ ))
-	    do
-	        printf "\n### SIEGE RUN %d OUT OF %d ###\n\n" "$i" "$WORKERS"
-
-          su - tester -c "cd django-workload/client                                    \
-                          && LOG='/tmp/siege.log' ./run-siege --single"
-	    done
-else
-
-      su - tester -c "cd django-workload/client                                    \
-                     && LOG='/tmp/siege.log' ./run-siege"
-fi
+                && ./gen-urls-file                                           \
+                && LOG='/tmp/siege.log' ./run-siege"
